7a2aa87cf9e4d9034f3e186fc9a3a8ab464c20fe
tekai-channel <76266501+tekai-channel@users.noreply.github.com>
2020-12-19 22:33:56 +0100

    1 Basic operations & Variables

---
+++ 1_basic_operations.py
@@ -0,0 +1,39 @@
+"""
+This script is part of the first tutorial "Python - Beginners | 1. Basic operations & Variables"
+"""
+
+# Create variable 'height'
+height = 170
+
+# Create variable 'weight'
+weight = 68
+
+# Display the variable
+print("Height          :", height)
+print("Weight          :", weight)
+
+# Basic operations with 'height' and 'weight'
+print("Height + Weight :", height+weight)
+print("Height ** 2     :", height**2)
+print("IMC             :", weight / ((height/100) ** 2) )
+
+
+# -- Exercice -- #
+# Goal :
+# > Write a program that compute the conversion from a Celcius temperature
+#   to a Fahrenheit temperature.
+# 
+# Formula :
+# > Tc = 
+
+# Give a value to 'Celcius' temperature
+Celcius = 0
+
+# Apply the given formula to 'Fahrenheit'
+Fahrenheit = 0
+
+# Display temperatures
+print()  # Line break
+print("Celcius    :", Celcius)
+print("Fahrenheit :", Fahrenheit)
+
